198cabc43945df6f99a1296b8e423b59040cc601
fremag <fremag.gh@gmail.com>
2020-04-13 21:27:04 +0200

    first features to test Genshi, Bootstrap et NetworkX

--- Server.py
+++ Server.py
@@ -1,22 +1,62 @@
+import json
 import cherrypy
 import os
+import networkx as nx
 from genshi.template import TemplateLoader
 
 
-class GraphServer(object):
+class GraphModel(object):
+    def __init__(self, graph):
+        self.graph = graph
+
+    def suc(self, name):
+        return self.graph.successors(name)
+
+    def dom(self, name):
+        dom = nx.immediate_dominators(self.graph, name)
+        return [n[0] for n in dom]
+
+
+class JsonServer(object):
     def __init__(self):
-        self.graph = None
+        self.model = None
+
+    def init(self, graph):
+        self.model = GraphModel(graph)
+        pass
+
+    @cherrypy.expose
+    def dom_json(self, name):
+        return json.dumps(self.model.dom(name))
+
+    @cherrypy.expose
+    def suc_json(self, name):
+        result = self.model.suc(name)
+        l = []
+        for n in result:
+            l.append(n)
+        return json.dumps(l)
+
+
+class HtmlServer(object):
+    def __init__(self):
+        self.model = None
 
         self.loader = TemplateLoader(
             os.path.join(os.path.dirname(__file__), 'templates'),
             auto_reload=True
-    )
+        )
+
+    def init(self, graph):
+        self.model = GraphModel(graph)
+        pass
 
     @cherrypy.expose
     def index(self):
         template = self.loader.load('index.html')
-        return template.generate(title='Graph Explorer').render('html', doctype='html' )
+        return template.generate(title='Graph Explorer', nodes=self.model.graph.nodes).render('html', doctype='html')
 
-    def init(self, graph):
-        self.graph = graph
-        pass
+    @cherrypy.expose
+    def node(self, name):
+        template = self.loader.load('node.html')
+        return template.generate(title="Node info", node=name).render('html', doctype='html')
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import cherrypy
 import networkx as nx
 import matplotlib.pyplot as plt
-from Server import GraphServer
+from Server import HtmlServer
+from Server import JsonServer
 
 G = nx.DiGraph()
 G.add_node('A')
@@ -27,30 +28,55 @@ G.add_edge('F', 'I')
 
 # G = nx.read_adjlist("e:/tmp/data2.txt", create_using=nx.DiGraph)
 
+
+print("**************************************************")
+print("* Dominance                                      *")
+print("**************************************************")
 dom = nx.dominating_set(G)
 
 for n in G:
     imm_dom = nx.immediate_dominators(G, n)
     if len(imm_dom) > 0:
-        print "Node: %s : %i" % (n, len(imm_dom) - 1)
-        print sorted([s[0] for s in imm_dom if s[0] != n])
+        print("Node: %s : %i" % (n, len(imm_dom) - 1))
+#        print(sorted([s[0] for s in imm_dom if s[0] != n]))
+
+print("**************************************************")
+print("* Ranks                                          *")
+print("**************************************************")
+#ranks = nx.voterank(G)
+#print(ranks)
+
+print("**************************************************")
+print("* Histogram                                      *")
+print("**************************************************")
+histo = nx.degree_histogram(G)
+print(histo)
+
+
+print("**************************************************")
+print("* Info                                           *")
+print("**************************************************")
+info = nx.info(G)
+#print(info)
+
 
 # nx.draw_spectral(G, with_labels=True)
 # plt.show()
 
-server = GraphServer()
-server.init(G)
+html_server = HtmlServer()
+html_server.init(G)
+
+json_server = JsonServer()
+json_server.init(G)
+
 cherrypy.config.update({
     'tools.encode.on': True, 'tools.encode.encoding': 'utf-8',
     'tools.decode.on': True,
     'tools.trailing_slash.on': True,
     'tools.staticdir.root': os.path.abspath(os.path.dirname(__file__)),
 })
+# exit()
+cherrypy.quickstart(html_server, '/')
+cherrypy.quickstart(json_server, '/json')
 
-cherrypy.quickstart(server, '/', {
-    '/media': {
-        'tools.staticdir.on': True,
-        'tools.staticdir.dir': 'static'
-    }
-})
 
