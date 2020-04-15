d84f2225f36dd56b3b509e4da541e22326650cf0
fremag <fremag.gh@gmail.com>
2020-04-15 17:08:55 +0200

    Add node type information

--- HtmlServer.py
+++ HtmlServer.py
@@ -25,6 +25,16 @@ class HtmlServer(object):
         return template.generate(title='Graph Explorer', nodes=self.model.graph.nodes, model=self.model).render('html', doctype='html')
 
     @cherrypy.expose
+    def types(self):
+        template = self.loader.load('types.html')
+        return template.generate(title='Type stats').render('html', doctype='html')
+
+    @cherrypy.expose
+    def type(self, type_name):
+        template = self.loader.load('type.html')
+        return template.generate(title="Type info", type_name=type_name).render('html', doctype='html')
+
+    @cherrypy.expose
     def node(self, name):
         template = self.loader.load('node.html')
         return template.generate(title="Node info", node=name).render('html', doctype='html')
--- JsonServer.py
+++ JsonServer.py
@@ -1,4 +1,5 @@
 import cherrypy
+import collections as col
 
 from GraphModel import GraphModel
 
@@ -29,6 +30,34 @@ class JsonServer(object):
         nodes = list(self.model.graph.nodes(data='type'))
         data = []
         for n in nodes:
-            node = {"name": n[0], "url": ("/node/%s" % n[0]), "type": n[1]}
+            node = {"name": n[0], "url": ("/node/%s" % n[0]), "type": n[1], "type_url": ("/type/%s" % n[1])}
+            data.append(node)
+        return data
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def types(self):
+        nodes = list(self.model.graph.nodes(data='type'))
+        stats = col.defaultdict(int)
+        for n in nodes:
+            node_type = n[1]
+            nb = stats[node_type]
+            nb += 1
+            stats[node_type] = nb
+
+        data = []
+        for node_type in stats:
+            s = {"type": node_type, "url": ("/type/%s" % node_type), "count": stats[node_type]}
+            data.append(s)
+
+        return data
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def type(self, type_name):
+        nodes = [n for n in list(self.model.graph.nodes(data='type')) if n[1] == type_name]
+        data = []
+        for n in nodes:
+            node = {"name": n[0], "url": ("/node/%s" % n[0]), "type": n[1], "type_url": ("/type/%s" % n[1])}
             data.append(node)
         return data
